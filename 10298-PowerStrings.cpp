a2088f3079bd09104584a01a07851f33259d85ed
Alexander Faxå <alexander.faxa@gmail.com>
2014-01-21 13:22:00 +0100

    UVa 100: timeout

---
+++ 10298-PowerStrings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <iterator>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+  vector<int> v;
+  for (int i : v) {
+    cout << i << endl;
+  }
+
+
+}
