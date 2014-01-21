83693a653b205f54c7e87e95540bddc238169753
Alexander Faxå <alexander.faxa@gmail.com>
2014-01-21 13:46:40 +0100

    UVa 10298: Compiling

--- 10298-PowerStrings.cpp
+++ 10298-PowerStrings.cpp
@@ -11,12 +11,29 @@
 
 using namespace std;
 
-int main() {
-
-  vector<int> v;
-  for (int i : v) {
-    cout << i << endl;
+bool solve(const string& s, int l) {
+  string ss = s.substr(0, l);
+  for (int i = 0; i < s.length(); i+=l) {
+    if (s.find(ss, i) != i) return false;
   }
+  return true;
+}
 
+int main() {
+  string s;
+  while (1) {
+    cin >> s;
+    if (s == ".") break;
+
+    int n = 1+s.length();
+
+    while (n--) {
+      if (s.length() % n != 0)  continue;
+      if (solve(s, s.length()/n)) {
+	cout << n << endl;
+	break;
+      }
+   }
+  }
 
 }
