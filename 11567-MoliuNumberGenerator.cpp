e7af66d7b7e80452109d66c98fca3a983fcca1bf
Alexander Faxå <alexander.faxa@gmail.com>
2014-02-04 18:33:39 +0100

    UVa 11567: Accepted

--- 11567-MoliuNumberGenerator.cpp
+++ 11567-MoliuNumberGenerator.cpp
@@ -12,30 +12,27 @@
 
 using namespace std;
 
-#define MAX_N 101
-#define EPS 1e-9
-#define INF 0x7FFFFFFF
-
-#define ii pair<int, int>
-#define vi vector<int>
+int low[5] = {1, 2, 3, 3, 4};
 
 int main() {
   long long t;
   while (cin >> t) {
-    long long n1 = 1;
-    long long n2 = 2;
-    long long cnt = 1;
+    long long cnt = 0;
+    while (t > 0) {
+      if (t < 6) {
+        cnt += low[t-1];
+        break;
+      }
 
-    while (n2 <= t) {
-      n1 = n2;
-      n2 *= 2;
+      if (t % 2 == 0)
+        t /= 2;
+      else if ( ( (t-1) / 2 ) % 2 == 0)
+        t--;
+      else
+        t++;
       cnt++;
     }
-
-    cout << cnt + min(t-n1, 1+n2-t) << endl;
-
+    cout << cnt << endl;
   }
-
-
   return 0;
 }
