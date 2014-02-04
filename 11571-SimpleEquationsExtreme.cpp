40c2980de9a9804498095dd7224f5819189af8a1
Alexander Faxå <alexander.faxa@gmail.com>
2014-02-04 20:44:20 +0100

    UVa 11565: Accepted

--- 11571-SimpleEquationsExtreme.cpp
+++ 11571-SimpleEquationsExtreme.cpp
@@ -15,15 +15,53 @@ using namespace std;
 
 #define SUPER_MAX 6000000000000000000
 
-long long a, b, c;
-long double A, B, C, up, low;
+#define EPS 1e-12
+
+long double a, b, c;
+long double A, B, C;
+
+inline long double myabs(long double n) {
+  return n < 0 ? -n : n;
+}
+
+inline bool IsInteger(long double n)
+{
+  n = (n < 0) ? -n : n;
+  return n - floor(n) < EPS;
+}
+
+inline bool isSolution() {
+  c = A - a - b;
+  if (IsInteger(b) && b > a && c > b) {
+    if ( myabs(C - a*a - b*b - c*c) < EPS) return true;
+  }
+  return false;
+}
+
+bool calculate_b(long double& kvot, long double& root) {
+  kvot = (a - A) / 2;
+  const long double sqkvot = kvot * kvot;
+  const long double Bbya = B/a;
+  if (Bbya >= sqkvot) return false;
+  root = sqrtl(sqkvot - Bbya);
+  return true;
+}
 
 bool solve() {
+  a = - min(floor(sqrtl(B)), floor(sqrtl(C))) - 2;
+  long double up = pow(B, 0.34) + 2;
+  long double kvot, root;
 
+  for (; a < up; a += 1.0) {
+    if (myabs(a) < EPS) continue;
+    if (!calculate_b(kvot,root)) continue;
 
-
+    b = -kvot + root;
+    if (isSolution()) return true;
+    b = -kvot - root;
+    if (isSolution()) return true;
+  }
   return false;
-
 }
 
 int main() {
@@ -31,11 +69,11 @@ int main() {
   cin >> m;
   while (m--) {
     cin >> A >> B >> C;
-    up = pow(B, 0.34) + 2;
-    low = - min(sqrtl(B), sqrtl(C)) - 2;
-    if (solve())
-      cout << a << " " << b << " " << c << endl;
-    else
+
+    if (solve()) {
+      cout.precision(0);
+      cout << fixed << (a+EPS) << " " << (b+EPS) << " " << (c+EPS) << endl;
+    } else
       cout << "No solution." << endl;
   }
   return 0;
