9441dd1bae95b0898c5954d5749ef00e6d74a494
Alexander Faxå <alexander.faxa@gmail.com>
2014-02-07 22:08:37 +0100

    UVa 793: Received

--- 793-NetworkConnections.cpp
+++ 793-NetworkConnections.cpp
@@ -24,10 +24,9 @@ int main() {
     first = false;
     int c;
     cin >> c;
-    for (int i = 0; i < c; i++) v[i] = -1;
+    for (int i = 0; i < c; i++) v[i] = i;
     int good = 0;
     int bad = 0;
-    int nbr_sec = 0;
     string s;
     getline(cin, s);
     while (1) {
@@ -39,25 +38,16 @@ int main() {
       ss >> d >> a >> b;
 
       if (d == 'q') {
-        if (v[a-1] == v[b-1] && v[a-1] != -1) good++;
+        if (v[a-1] == v[b-1]) good++;
         else bad++;
       } else {
-        if (v[a-1] == -1 && v[b-1] != -1)
-          v[a-1] = v[b-1];
-        else if (v[a-1] != -1 && v[b-1] == -1)
-          v[b-1] = v[a-1];
-        else if (v[a-1] == -1 && v[b-1] == -1) {
-          v[a-1] = nbr_sec;
-          v[b-1] = nbr_sec;
-          nbr_sec++;
-        } else {
-          int temp = v[a-1];
-          for (int i = 0; i < c; i++) {
-            if (v[i] == temp) v[i] = v[b-1];
-          }
+        int temp = v[a-1];
+        for (int i = 0; i < c; i++) {
+          if (v[i] == temp) v[i] = v[b-1];
         }
       }
     }
+
     cout << good << "," << bad << endl;
   }
   return 0;
